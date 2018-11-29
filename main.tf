f58e7b336c3ecbd9bc1d2f80dfe0dbe77ce36ba2
Dan DeFelippi <dan@driverdan.com>
2018-11-29 11:35:15 -0500

    Add terraform

---
+++ main.tf
@@ -0,0 +1,6 @@
+provider "heroku" {
+  version = "~> 1.5"
+}
+provider "cloudflare" {
+  version = "~> 1.9"
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "app_name" {
+  description = "Unique name of the app"
+  type = "string"
+  default = "dan-36"
+}
+
+variable "custom_domain" {
+  description = "Custom domain name (optional)"
+  type = "string"
+  default = ""
+}
